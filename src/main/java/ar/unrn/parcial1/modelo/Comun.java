ea490cd6798da687e68fc9613a1d7a931a14d4f5
Daiana <alonsodaiana5@gmail.com>
2021-05-26 19:15:12 -0300

    Final

--- src/main/java/ar/unrn/parcial1/modelo/Comun.java
+++ src/main/java/ar/unrn/parcial1/modelo/Comun.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 
 public class Comun extends Combustible {
     private static final double PRECIO = 70;
+    private static final double DESCUENTO_AM = 0.05;
+    private static final double HORA_INICIO = 8;
+    private static final double HORA_LIMITE = 10;
 
     public Comun() {
     }
@@ -11,12 +14,12 @@ public class Comun extends Combustible {
     public double obtenerMonto(double litros, LocalDateTime fecha) {
         double montoTotal = PRECIO * litros;
         if (cargoALaManiana(fecha))
-            montoTotal = montoTotal - (0.05 * montoTotal);
+            montoTotal = montoTotal - (DESCUENTO_AM * montoTotal);
         return montoTotal;
     }
 
     private boolean cargoALaManiana(LocalDateTime fecha) {
-        return (fecha.getHour() >= 8 && fecha.getHour() <= 10);
+        return (fecha.getHour() >= HORA_INICIO && fecha.getHour() <= HORA_LIMITE);
     }
 
 }
--- src/main/java/ar/unrn/parcial1/modelo/Super.java
+++ src/main/java/ar/unrn/parcial1/modelo/Super.java
@@ -5,6 +5,10 @@ import java.time.LocalDateTime;
 
 public class Super extends Combustible {
     private static final double PRECIO = 90;
+    private static final double DESCUENTO_DOMINGO = 0.1;
+    private static final double SUPERAR_LITROS = 20;
+    private static final double DESCUENTO_SABADO = 0.12;
+
 
     public Super() {
     }
@@ -12,9 +16,9 @@ public class Super extends Combustible {
     public double obtenerMonto(double litros, LocalDateTime fecha) {
         double montoTotal = PRECIO * litros;
         if (esDomingo(fecha))
-            montoTotal = montoTotal - (0.1 * montoTotal);
-        if (esSabado(fecha) && litros > 20)
-            montoTotal = montoTotal - (0.12 * montoTotal);
+            montoTotal = montoTotal - (DESCUENTO_DOMINGO * montoTotal);
+        if (esSabado(fecha) && litros > SUPERAR_LITROS)
+            montoTotal = montoTotal - (DESCUENTO_SABADO * montoTotal);
         return montoTotal;
     }
 
